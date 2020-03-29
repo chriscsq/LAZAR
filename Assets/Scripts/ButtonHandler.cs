5d69bf71332c4cb85b749f1eff3b9e8327721338
chriscsq <chrischau94@gmail.com>
2020-03-29 16:21:21 -0600

    Updated scene

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void NextScene() 
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
