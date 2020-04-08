0afb520626ec3be373940c8c894eb08f345c3d24
chriscsq <chrischau94@gmail.com>
2020-04-07 21:40:20 -0600

    added screen

---
+++ Assets/Scripts/ButtonHandler 2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void NextScene() 
+    {
+        Debug.Log("Switching to samplescene");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
