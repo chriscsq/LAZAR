7647daf2a0bc24af78eeab18faf097aa1f7ce16d
chriscsq <chrischau94@gmail.com>
2020-04-10 18:42:22 -0600

    Setting progress

---
+++ Assets/Scripts/HomePageBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePageBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject HomeScreen;
+    [SerializeField]
+    private GameObject SettingScreen;
+    [SerializeField]
+    private GameObject LoadingScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start(), homefully will set active to false");
+        HomeScreen.SetActive(true);
+        SettingScreen.SetActive(false);
+        LoadingScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SettingButtonBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingButtonBehavior : MonoBehaviour
+{
+
+    public GameObject HomeScreen;
+    public GameObject SettingScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackToHome()
+    {
+        SettingScreen.SetActive(false);
+        HomeScreen.SetActive(true);
+    }
+}
