291b080ba9be93b5d54e993518bbf75922b6e4b4
chriscsq <chrischau94@gmail.com>
2020-04-15 21:30:11 -0600

    Added how to play screen

---
+++ Assets/Scripts/GameScreenButtonBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScreenButtonBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject overlay;
+    [SerializeField]
+    private GameObject LoadScreen;
+
+    public void BackToHomeScreen()
+	{
+        SceneManager.LoadScene("HomeScreen");
+        LoadScreen.SetActive(false);
+
+    }
+
+    public void BeginPlayGame()
+	{
+        Debug.Log("Play game started");
+        overlay.SetActive(false);
+	}
+}
